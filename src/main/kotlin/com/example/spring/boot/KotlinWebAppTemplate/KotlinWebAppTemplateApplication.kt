35022d07e28039d7b6c83af6271c83029208fe52
sohatach <sohatach@gmail.com>
2018-06-18 00:01:19 +0900

    generate project

---
+++ src/main/kotlin/com/example/spring/boot/KotlinWebAppTemplate/KotlinWebAppTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring.boot.KotlinWebAppTemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebAppTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebAppTemplateApplication>(*args)
+}
